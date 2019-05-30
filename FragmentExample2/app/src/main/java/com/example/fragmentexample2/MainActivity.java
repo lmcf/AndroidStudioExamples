2f0b8543644b57aed788dc6c3030b6b8d923b45c
caldeiro <caldeiro@protonmail.com>
2019-05-30 02:04:53 +0200

    Fragment Example 2
    Example two fragments with menu

---
+++ FragmentExample2/app/src/main/java/com/example/fragmentexample2/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.fragmentexample2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+// Has de implementas el itemfragment
+public class MainActivity extends AppCompatActivity implements
+        itemfragment1.onFragmentInteraction, FragmentBienvenida.OnFragmentInteractionListener{
+
+    FragmentBienvenida bienvenido;
+    itemfragment1 pantalla1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Asociamos variables a fragments
+        bienvenido = new FragmentBienvenida();
+        pantalla1 = new itemfragment1();
+
+        //Para mostrar 1 frament nada mas empezar, hacemos esto, NO OLVIDAR COMMIT()
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,bienvenido).commit();
+
+    }
+
+
+    // Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fm = getSupportFragmentManager();
+        switch (item.getItemId()){
+            case R.id.fragment1:
+                Fragment fragment = new itemfragment1().newInstance("parametro1","parametro2","parametro3");
+                fm.beginTransaction().replace(R.id.fragment_container,fragment).commit();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // Cunaod no usamos metodos del controller sin nada
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
+}
